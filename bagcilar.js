4083e0ec6b40e7ce15fc789f695b7e1586c6a3a6
hwclass <harderworking.class@gmail.com>
2015-04-25 23:32:03 +0300

    Edited indentation.

--- bagcilar.js
+++ bagcilar.js
@@ -22,47 +22,47 @@ var bagcilar = (function () {
 
 	return {
 
-		/**
-		 * module() is a module creation method
-		 *
-		 * @param <String> name
-		 * @param <Function> fn
-		 * @param <Boolean> initOnLoad
-		 */
-		module : function (name, fn, initOnLoad) {
-			MODULES[name] = new fn();
-			if (name === mainModule) {
-				this.init(mainModule);
-			} else {
-				if (initOnLoad) {
-					this.start(name);
-				}
-			}
-		},
+    /**
+    * module() is a module creation method
+    *
+    * @param <String> name
+    * @param <Function> fn
+    * @param <Boolean> initOnLoad
+    */
+    module : function (name, fn, initOnLoad) {
+      MODULES[name] = new fn();
+      if (name === mainModule) {
+        this.init(mainModule);
+      } else {
+        if (initOnLoad) {
+          this.start(name);
+        }
+      }
+    },
 
-		/**
-		 * start() is a module initializing method
-		 * that makes modules starting to work
-		 *
-		 * @param <String> name
-		 */
-		start : function (name) {
-			for(var key in MODULES[name]) {
-				if (MODULES[name][key] instanceof Function) {
-					MODULES[name][key]();
-				}
-			}
-		},
+    /**
+    * start() is a module initializing method
+    * that makes modules starting to work
+    *
+    * @param <String> name
+    */
+    start : function (name) {
+      for(var key in MODULES[name]) {
+        if (MODULES[name][key] instanceof Function) {
+          MODULES[name][key]();
+        }
+      }
+    },
 
-			/**
-			 * init() is a main module initializing method
-			 * that makes modules starting to work
-			 *
-			 * @param <String> mainModule
-			 */
-			init : function(mainModule){
-				MODULES[mainModule]['init'];
-			}
+    /**
+    * init() is a main module initializing method
+    * that makes modules starting to work
+    *
+    * @param <String> mainModule
+    */
+    init : function(mainModule){
+      MODULES[mainModule]['init'];
+    }
 
 	}
 
